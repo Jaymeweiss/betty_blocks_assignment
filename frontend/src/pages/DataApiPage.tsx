9bee73f6e90183c40b7e4af8aa9439b6d17572ff
jaymeweiss <jaymeweiss@icloud.com>
2025-08-11 07:47:39 +0200

    add wip changes for displaying data in DataApi

--- frontend/src/pages/DataApiPage.tsx
+++ frontend/src/pages/DataApiPage.tsx
@@ -1,8 +1,109 @@
+import {useEffect, useState} from "react";
+import {dataApi, type DatabaseTableDataResponse, type DatabaseTableListResponse} from "../services/dataApi.tsx";
+
 function DataApiPage() {
-    const style = { padding: "8px" };
+    const [isRetrievingDatabaseTableList, setIsRetrievingDatabaseTableList] = useState(false);
+    const [isRetrievingData, setIsRetrievingData] = useState(false);
+    const [databaseList, setDatabaseList] = useState<string[]>([]);
+    const [selectedDatabaseTable, setSelectedDatabaseTable] = useState<string | null>(null);
+    const [columns, setColumns] = useState<string[]>([]);
+    const [rows, setRows] = useState<string[][]>([]);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    const handleRetrieveDatabaseTableList = async () => {
+        setIsRetrievingDatabaseTableList(true);
+        setErrorMessage(null);
+        setDatabaseList([]);
+        setSelectedDatabaseTable(null);
+        setColumns([]);
+        setRows([]);
+
+        try {
+            const response = await dataApi.getDatabaseTableList();
+            const result: DatabaseTableListResponse = await response.json();
+            if (response.ok && response.status === 200) {
+                setDatabaseList(result.database_tables);
+            } else {
+                setErrorMessage("Failed to retrieve database table list");
+            }
+        } catch (_error) {
+            setErrorMessage("Failed to connect to the data API. Please try again.");
+        } finally {
+            setIsRetrievingDatabaseTableList(false);
+        }
+    };
+
+    const handleRetrieveData = async (database: string) => {
+        setIsRetrievingData(true);
+        setSelectedDatabaseTable(database);
+        setErrorMessage(null);
+        setColumns([]);
+        setRows([]);
+
+        try {
+            const response = await dataApi.getData(database);
+            const result: DatabaseTableDataResponse = await response.json();
+            if (response.ok && response.status === 200) {
+                setColumns(result.columns);
+                setRows(result.rows);
+            } else {
+                setErrorMessage("Failed to retrieve data");
+            }
+        } catch (_error) {
+            setErrorMessage("Failed to connect to the data API. Please try again.");
+        } finally {
+            setIsRetrievingData(false);
+        }
+    };
+
+    useEffect(() => {
+        handleRetrieveDatabaseTableList();
+    }, []);
+
     return (
-        <div style={style}>
+        <div>
             <h1>Data API</h1>
+            {errorMessage && <div className="error-message">{errorMessage}</div>}
+            <div>
+                {isRetrievingDatabaseTableList && (
+                    <p>Retrieving database table list...</p>
+                )}
+                <div className={databaseList ? "database-list" : "database-list hidden"}>
+                    <h2>Databases:</h2>
+                    <div className="chip-container">
+                        {databaseList.map((database, index) => (
+                            <div
+                                key={index}
+                                className="chip"
+                                onClick={() => handleRetrieveData(database)}
+                            >
+                                {database}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                {isRetrievingData && <p>Retrieving data...</p>}
+                <div className={columns ? "table-container" : "table-container hidden"}>
+                    <table>
+                        <thead>
+                        <tr>
+                            {columns.map((column, index) => (
+                                <th key={index}>{column}</th>
+                            ))}
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {rows.map((row, rowIndex) => (
+                            <tr key={`${selectedDatabaseTable}-${rowIndex}`}>
+                                {row.map((cell, cellIndex) => (
+                                    <td key={`${rowIndex}-${cellIndex}`}>{cell}</td>
+                                ))}
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ frontend/src/services/dataApi.tsx
@@ -0,0 +1,39 @@
+import API_CONFIG from '../config/api';
+
+export interface DatabaseTableListResponse {
+    database_tables: string[];
+}
+
+export interface DatabaseTableDataResponse {
+    columns: string[];
+    rows: string[][]; // data types here need to be more specific - we shouldn't just be returning strings
+}
+
+class DataApiService {
+    private readonly baseUrl: string;
+
+    constructor() {
+        this.baseUrl = API_CONFIG.DATA_API_BASE_URL;
+    }
+
+    async getDatabaseTableList(): Promise<Response> {
+        return await fetch(`${this.baseUrl}/databases`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    }
+
+    async getData(databaseTable: string): Promise<Response> {
+        return await fetch(`${this.baseUrl}/data/${databaseTable}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    }
+
+}
+
+export const dataApi = new DataApiService();
